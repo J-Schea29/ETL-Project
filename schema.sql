e02ed438f871a5b9264299cca5617a6f69dc0198
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-09-21 21:50:48 -0700

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,31 @@
+---CREATE TABLES FOR CSV
+
+---Table for Region Data
+CREATE TABLE region (
+  region_id INT PRIMARY KEY,
+  country TEXT,
+  province TEXT,
+  Region TEXT,
+);
+
+---Table for Weather Data
+CREATE TABLE weather (
+  region_id INT NOT NULL,
+  Lat INT NOT NULL,
+  Lng INT NOT NULL,
+  max_temp INT NOT NULL,
+  min_temp INT NOT NULL,
+  Temp INT NOT NULL
+  Humidity INT NOT NULL
+  diurnal_range INT NOT NULL,
+  FOREIGN KEY (region_id) REFERENCES region (region_id)  
+);
+
+---Table for Wine Data
+CREATE TABLE wine (
+  region_id INT NOT NULL,
+  variety TEXT,
+  points INT NOT NULL,
+  price INT NOT NULL,
+  FOREIGN KEY (region_id) REFERENCES region (region_id)
+);
